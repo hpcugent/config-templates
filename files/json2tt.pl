9221de7f79862af33877919554efd3ef5d3f2931
stdweird <stijn.deweirdt@ugent.be>
2013-05-15 20:47:14 +0200

    allow arbitrary profile path

--- files/json2tt.pl
+++ files/json2tt.pl
@@ -11,9 +11,12 @@ use Template;
 ## ./json2tt.pl /path/to/file/with/json/snippet path/to/template.tt
 ##
 ## support reading quattor json profiles directly and using the contents from metaconfig component
-##   metaconfig path is 4th argumnet, use empty string as 3rd if needed
-##  ./json2tt.pl /path/to/quattor/build/xml/node.domain.json.gz metaconfig/snmp/snmp.tt '' /etc/snmp/snmp.conf
-##
+##   4th argument, use empty string as 3rd if needed
+##   absolute path: metaconfig filename path
+##      ./json2tt.pl /path/to/quattor/build/xml/node.domain.json.gz metaconfig/snmp/snmp.tt '' /etc/snmp/snmp.conf
+##   relative path: path in tree
+##      ./json2tt.pl /path/to/quattor/build/xml/node.domain.json.gz authconfig.sssd.tt '' software/components/authconfig/method/sssd
+
 
 sub escape($) {
     my $str = shift;
@@ -34,8 +37,18 @@ my $json = decode_json(join("", <$fh>));
 close($fh);
 
 if ( @ARGV >= 4 ) {
-    $json = $json->{software}->{components}->{metaconfig}->{services}->{escape($ARGV[3])}->{contents};
+    if ($ARGV[3] =~ m/^\//) {
+        # abs path for file in metaconfig
+        $json = $json->{software}->{components}->{metaconfig}->{services}->{escape($ARGV[3])}->{contents};
+    } else {
+        # rel path, starting from root of tree
+        my @paths = split('/', $ARGV[3]);
+        foreach my $path (@paths) {
+            $json = $json->{$path};
+        }
+    }
 };
+
 
 my $tpl = Template->new(STRICT => $ARGV[2]);
 $tpl->process($ARGV[1], $json) or die "WTF: ", $tpl->error();
